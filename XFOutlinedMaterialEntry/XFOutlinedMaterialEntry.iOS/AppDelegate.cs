89f96b33175830a9e61edf7cae77d3015e569048
hmji <hmji@nansoft.co.kr>
2023-07-13 16:57:31 +0900

    Add project files.

---
+++ XFOutlinedMaterialEntry/XFOutlinedMaterialEntry.iOS/AppDelegate.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using UIKit;
+
+namespace XFOutlinedMaterialEntry.iOS
+{
+    [Register(nameof(AppDelegate))]
+    public partial class AppDelegate : Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
